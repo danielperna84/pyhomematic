af3da7ad6c7d1c0b4ae7a8f10f06ee2fcb1c9c2f
Daniel <danielperna84@gmail.com>
2016-03-01 21:45:34 +0000

    Minor modifications for PyPi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'pyhomematic',
-  packages = ['pyhomematic'], # this must be the same as the name above
-  version = '0.1',
+  packages = ['homematic'],
+  version = '0.0.1',
   description = 'Homematic interface',
   author = 'Daniel Perna',
   author_email = 'danielperna84@gmail.com',
   license='MIT',
-  url = 'https://github.com/danielperna84/pyhomematic', # use the URL to the github repo
-  download_url = 'https://github.com/danielperna84/pyhomematic/tarball/0.1', # I'll explain this in a second
-  keywords = ['homematic'], # arbitrary keywords
+  url = 'https://github.com/danielperna84/pyhomematic',
+  download_url = 'https://github.com/danielperna84/pyhomematic/tarball/0.1',
+  keywords = ['homematic', 'automation', 'smarthome'],
   classifiers = [],
 )
