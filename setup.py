19897d26b0d44bb5831a8c13b66dda08be448638
Daniel <danielperna84@gmail.com>
2016-03-01 23:31:33 +0000

    Changes for packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
+
+def readme():
+  with open('README.rst') as f:
+    return f.read()
+
 setup(
   name = 'pyhomematic',
   packages = ['homematic'],
   version = '0.0.1',
   description = 'Homematic interface',
+  long_description=readme(),
   author = 'Daniel Perna',
   author_email = 'danielperna84@gmail.com',
   license='MIT',
   url = 'https://github.com/danielperna84/pyhomematic',
-  download_url = 'https://github.com/danielperna84/pyhomematic/tarball/0.1',
+  download_url = 'https://github.com/danielperna84/pyhomematic/tarball/0.0.1',
   keywords = ['homematic', 'automation', 'smarthome'],
-  classifiers = [],
+  classifiers = ["Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers"]
 )
