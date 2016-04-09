49aab1d215accf39510110dac64677a1fceb6f80
Daniel <danielperna84@gmail.com>
2016-04-09 11:07:02 +0000

    Example uses modified packagename

--- example.py
+++ example.py
@@ -10,13 +10,13 @@ def systemcallback(src, *args):
     for arg in args:
         print(arg)
 
-import homematic
+import pyhomematic
 try:
     # Create a server that listens on 127.0.0.1:7080 and identifies itself as myserver.
     # Connect to Homegear at 127.0.0.1:2001
-    # Automatically start everything. Without autostart, homematic.start() can be called.
+    # Automatically start everything. Without autostart, pyhomematic.start() can be called.
     # We add a systemcallback so we can see what else happens besides the regular events.
-    homematic.create_server(interface_id="myserver",
+    pyhomematic.create_server(interface_id="myserver",
                             local="127.0.0.1",
                             localport=7080,
                             remote="127.0.0.1",
@@ -31,38 +31,38 @@ sleepcounter = 0
 def eventcallback(address, interface_id, key, value):
     print("CALLBACK: %s, %s, %s, %s" % (address, interface_id, key, value))
 
-if homematic.Server:
-    while not homematic.devices and sleepcounter < 20:
+if pyhomematic.Server:
+    while not pyhomematic.devices and sleepcounter < 20:
         print("Waiting for devices")
         sleepcounter += 1
         time.sleep(1)
-    print(homematic.devices)
+    print(pyhomematic.devices)
     
     # Get level of rollershutter from 0.0 to 1.0.
-    print(homematic.devices[DEVICE1].level)
+    print(pyhomematic.devices[DEVICE1].level)
     
     # Set level of rollershutter to 50%.
-    homematic.devices[DEVICE1].level = 0.5
+    pyhomematic.devices[DEVICE1].level = 0.5
     time.sleep(10)
     
     # Move rollershutter down.
-    homematic.devices[DEVICE1].move_down()
+    pyhomematic.devices[DEVICE1].move_down()
     time.sleep(10)
     
     # Get level of rollershutter from 0.0 to 1.0 directly from channel.
-    print(homematic.devices_all[DEVICE1 + ':1'].getValue("LEVEL"))
+    print(pyhomematic.devices_all[DEVICE1 + ':1'].getValue("LEVEL"))
     
     # Check if doorcontact is open by querying the device.
-    print(homematic.devices[DEVICE2].is_open)
+    print(pyhomematic.devices[DEVICE2].is_open)
     
     # Check if doorcontact is open or closed by querying the device-channel. True or False, depending on state.
-    print(homematic.devices_all[DEVICE2 + ':1'].getValue("STATE"))
+    print(pyhomematic.devices_all[DEVICE2 + ':1'].getValue("STATE"))
     
     # Set an eventcallback for the doorcontact that should be called when events occur.
-    homematic.devices[DEVICE2].setEventCallback(eventcallback)
+    pyhomematic.devices[DEVICE2].setEventCallback(eventcallback)
     time.sleep(10)
     # Now open / close doorcontact and watch the eventcallback being called.
     
     # Stop the server thread so Python can exit properly.
-    homematic.stop()
+    pyhomematic.stop()
 sys.exit(0)
