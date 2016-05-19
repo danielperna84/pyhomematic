06c81b239e40b20988affd3b57a49d7a07776fd9
Daniel <danielperna84@gmail.com>
2016-05-19 20:43:31 +0000

    Moved to setuptools because distutils didn't include subfolder

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 
 def readme():
   with open('README.rst') as f:
     return f.read()
 
+PACKAGE_NAME = 'pyhomematic'
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'ccu', 'build'])
+
+REQUIRES = []
+
 setup(
-  name='pyhomematic',
-  packages=['pyhomematic'],
-  version='0.1.0',
-  description='Homematic interface',
-  long_description=readme(),
-  author='Daniel Perna',
-  author_email='danielperna84@gmail.com',
-  license='MIT',
-  url='https://github.com/danielperna84/pyhomematic',
-  download_url='https://github.com/danielperna84/pyhomematic/tarball/0.1.0',
-  keywords=['homematic', 'automation', 'smarthome'],
-  classifiers=["Programming Language :: Python :: 3",
-    "Intended Audience :: Developers"],
-    platforms="Any"
+    name=PACKAGE_NAME,
+    version="0.1.1",
+    license='MIT License',
+    url='https://github.com/danielperna84/pyhomematic',
+    download_url='https://github.com/danielperna84/pyhomematic/tarball/0.1.1',
+    author='Daniel Perna',
+    author_email='danielperna84@gmail.com',
+    description='Homematic interface',
+    packages=PACKAGES,
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=REQUIRES,
+    keywords=['home', 'automation', 'homematic', 'smarthome'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Home Automation'
+    ],
 )
