00b3f6d9cb452d0b6e8f7e2527dc80c9533f7e06
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-17 00:34:20 +0200

    update device list

--- pyhomematic/devicetypes/sensors.py
+++ pyhomematic/devicetypes/sensors.py
@@ -34,7 +34,7 @@ class DefaultBinarySensor(HMBinarySensor):
 
 class ShutterContact(DefaultBinarySensor):
     """
-    HM-Sec-SC, HM-Sec-SC-2, ZEL STG RM FFK
+    HM-Sec-SC, HM-Sec-SC-2, ZEL STG RM FFK, HM-Sec-SCo
     Door / Window contact that emits its open/closed state.
     """
     def is_open(self):
@@ -65,16 +65,60 @@ class RotaryHandleSensor(DefaultBinarySensor):
 
 
 class Remote(HMBinarySensor):
-    pass
+    """
+    BRC-H, HM-RC-2-PBU-FM, HM-RC-Dis-H-x-EU, HM-RC-4, HM-RC-4-B, HM-RC-4-2,
+    HM-RC-4-3, HM-RC-4-3-D, HM-RC-8, HM-RC-12, HM-RC-12-B, HM-RC-12-SW,
+    HM-RC-19, HM-RC-19-B, HM-RC-19-SW, HM-RC-Key3, HM-RC-Key3-B, HM-RC-Key4-2,
+    HM-RC-Key4-3, HM-RC-Sec3, HM-RC-Sec3-B, HM-RC-Sec4-2, HM-RC-Sec4-3,
+    HM-RC-P1, HM-RC-SB-X, HM-RC-X, HM-PB-2-WM, HM-PB-4-WM, HM-PB-6-WM55
+    Remote handle buttons
+    """
+    def __init__(self, device_description, proxy, resolveparamsets=False):
+        super().__init__(self, device_description, proxy, resolveparamsets)
+
+        self.BINARYNODE.update({"STATE": 0})
 
 
 DEVICETYPES = {
     "HM-Sec-SC": ShutterContact,
     "HM-Sec-SC-2": ShutterContact,
+    "HM-Sec-SCo": ShutterContact,
     "ZEL STG RM FFK": ShutterContact,
     "HM-Sec-RHS": RotaryHandleSensor,
     "ZEL STG RM FDK": RotaryHandleSensor,
     "HM-Sec-RHS-2": RotaryHandleSensor,
     "HM-Sec-xx": RotaryHandleSensor,
-    "HM-RC-8": Remote
+    "HM-RC-8": Remote,
+    "BRC-H": Remote,
+    "HM-RC-2-PBU-FM": Remote,
+    "HM-RC-Dis-H-x-EU": Remote,
+    "HM-RC-4": Remote,
+    "HM-RC-4-B": Remote,
+    "HM-RC-4-2": Remote,
+    "HM-RC-4-3": Remote,
+    "HM-RC-4-3-D": Remote,
+    "HM-RC-8": Remote,
+    "HM-RC-12": Remote,
+    "HM-RC-12-B": Remote,
+    "HM-RC-12-SW": Remote,
+    "HM-RC-19": Remote,
+    "HM-RC-19-B": Remote,
+    "HM-RC-19-SW": Remote,
+    "HM-RC-Key3": Remote,
+    "HM-RC-Key3-B": Remote,
+    "HM-RC-Key4-2": Remote,
+    "HM-RC-Key4-3": Remote,
+    "HM-RC-Sec3": Remote,
+    "HM-RC-Sec3-B": Remote,
+    "HM-RC-Sec4-2": Remote,
+    "HM-RC-Sec4-3": Remote,
+    "HM-RC-P1": Remote,
+    "HM-RC-SB-X": Remote,
+    "HM-RC-X": Remote,
+    "HM-PB-2-WM": Remote,
+    "HM-PB-4-WM": Remote,
+    "HM-PB-6-WM55": Remote,
+    "RC-H": Remote,
+    "atent": Remote,
+    "ZEL STG RM HS 4": Remote
 }
