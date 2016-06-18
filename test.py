60e25eb30b0b8784f8a568536ceaa2a1d5ada213
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-18 14:28:15 +0200

    test script is now runnable

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+import time
+import sys
+import logging
 import click
 from pyhomematic import HMConnection
+
+logging.basicConfig(level=logging.INFO)
 
 def systemcallback(src, *args):
     print(src)
@@ -12,12 +17,12 @@ def eventcallback(address, interface_id, key, value):
 
 @click.command()
 @click.option("--local", "-l", help="Local address for server")
-@click.option("--localPort", "-lp", default=8201 help="Local Port for server")
+@click.option("--localPort", "-lp", default=8201, help="Local Port for server")
 @click.option("--remote", "-r", help="Remote address for CCU/homegear")
-@click.option("--remotePort", "-rp", default=2001 help="Remote port for CCU/homegear")
+@click.option("--remotePort", "-rp", default=2001, help="Remote port for CCU/homegear")
 @click.option("--address", "-a", help="Address of homematic device for tests")
-@click.option("--channel", "-c", default=1 help="Homematic device channel")
-@click.option("--time", "-t", default=30 help="Time in sec for waiting of events (debug)")
+@click.option("--channel", "-c", default=1, help="Homematic device channel")
+@click.option("--time", "-t", default=30, help="Time in sec for waiting of events (debug)")
 def cli(local, localPort, remote, remotePort, address, channel, time):
 
     try:
@@ -40,3 +45,10 @@ def cli(local, localPort, remote, remotePort, address, channel, time):
         sleepcounter += 1
         time.sleep(1)
     print(pyhomematic.devices)
+
+    # do nothing for show & debug events
+    time.sleep(time)
+
+
+if __name__ == "__main__":
+    cli()
