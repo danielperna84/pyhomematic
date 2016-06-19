90253cb2ece965f0e561396b9c668e7a58f98126
Daniel Perna <danielperna84@gmail.com>
2016-06-19 19:08:05 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ REQUIRES = []
 
 setup(
     name=PACKAGE_NAME,
-    version='0.1.3',
+    version='0.1.4',
     license='MIT License',
     url='https://github.com/danielperna84/pyhomematic',
-    download_url='https://github.com/danielperna84/pyhomematic/tarball/0.1.3',
+    download_url='https://github.com/danielperna84/pyhomematic/tarball/0.1.4',
     author='Daniel Perna',
     author_email='danielperna84@gmail.com',
     description='Homematic interface',
