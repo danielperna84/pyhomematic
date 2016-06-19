1da06fe4ae982000ae9ddc667699306a6592898e
Daniel Perna <danielperna84@gmail.com>
2016-06-19 19:12:57 +0200

    Added docstrings and HelperLevel

--- pyhomematic/devicetypes/helper.py
+++ pyhomematic/devicetypes/helper.py
@@ -2,8 +2,7 @@ from pyhomematic.devicetypes.generic import HMDevice
 
 
 class HelperSabotage(HMDevice):
-    """
-    """
+    """This helper adds sabotage detection."""
     def __init__(self, device_description, proxy, resolveparamsets=False):
         super().__init__(device_description, proxy, resolveparamsets)
 
@@ -11,13 +10,12 @@ class HelperSabotage(HMDevice):
         self.ATTRIBUTENODE.update({"ERROR": 'c'})
 
     def sabotage(self, channel=1):
-        """ Returns if the devicecase has been opened. """
+        """Returns True if the devicecase has been opened."""
         return bool(self.getAttributeData("ERROR", channel))
 
 
 class HelperLowBat(HMDevice):
-    """
-    """
+    """This Helper adds easy access to read the LOWBAT state"""
     def __init__(self, device_description, proxy, resolveparamsets=False):
         super().__init__(device_description, proxy, resolveparamsets)
 
@@ -30,8 +28,7 @@ class HelperLowBat(HMDevice):
 
 
 class HelperWorking(HMDevice):
-    """
-    """
+    """This helper provides access to the WORKING state of some devices."""
     def __init__(self, device_description, proxy, resolveparamsets=False):
         super().__init__(device_description, proxy, resolveparamsets)
 
@@ -43,24 +40,46 @@ class HelperWorking(HMDevice):
         return self.getAttributeData("WORKING", channel)
 
 
+class HelperLevel(HMDevice):
+    """
+    Generic dimmer / level functions
+    """
+    def __init__(self, device_description, proxy, resolveparamsets=False):
+        super().__init__(device_description, proxy, resolveparamsets)
+
+        # init metadata
+        self.WRITENODE.update({"LEVEL": 'c'})
+
+    def get_level(self, channel=1):
+        """Return current level. Return value is float() from 0.0 to 1.0."""
+        return self.getWriteData("LEVEL", channel)
+
+    def set_level(self, position, channel=1):
+        """Seek a specific value by specifying a float() from 0.0 to 1.0."""
+        try:
+            position = float(position)
+        except Exception as err:
+            LOG.debug("HMLevel.level: Exception %s" % (err,))
+            return False
+
+        self.writeNodeData("LEVEL", position, channel)
+
+
 class HelperBatteryState(HMDevice):
-    """
-    """
+    """View the current state of the devices battery if available."""
     def battery_state(self):
         """ Returns the current battery state. """
         return float(self.getAttributeData("BATTERY_STATE"))
 
 
 class HelperValveState(HMDevice):
-    """
-    """
+    """View the valve state of thermostats and valve controllers."""
     def valve_state(self):
         """ Returns the current valve state. """
         return int(self.getAttributeData("VALVE_STATE"))
 
 class HelperBinaryState(HMDevice):
-    """
-    """
+    """Return the state of binary sensors."""
     def __init__(self, device_description, proxy, resolveparamsets=False):
         super().__init__(device_description, proxy, resolveparamsets)
 
