e62653396098482435cf1f647461f31cf6417fc6
tringler <mail@thomas-ringler.de>
2017-01-17 18:06:31 +0100

    Fixed reference for BC-SC-Rd-WM

--- pyhomematic/devicetypes/sensors.py
+++ pyhomematic/devicetypes/sensors.py
@@ -346,7 +346,7 @@ DEVICETYPES = {
     "HM-Sec-SCo": ShutterContact,
     "ZEL STG RM FFK": ShutterContact,
     "BC-SC-Rd-WM-2": ShutterContact,
-    "BC-SC-Rd-WM": ShutterContact,
+    "BC-SC-Rd-WM": MaxShutterContact,
     "HM-SCI-3-FM": IPShutterContact,
     "HMIP-SWDO": IPShutterContact,
     "HM-Sec-RHS": RotaryHandleSensor,
