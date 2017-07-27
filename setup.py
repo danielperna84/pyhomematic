c4deff982b67a5f24529d5e9bc5e38c6e7bc5c64
Daniel Perna <danielperna84@gmail.com>
2017-07-27 16:30:24 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 PACKAGE_NAME = 'pyhomematic'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '0.1.29'
+VERSION = '0.1.30'
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'ccu', 'build'])
 
