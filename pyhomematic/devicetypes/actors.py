526dc7d963168921bca2f201dd4ab91eb0e6a546
Daniel Perna <danielperna84@gmail.com>
2020-02-20 00:52:51 +0100

    0.1.65 (#290)
    * Bump version
    
    * Add HmIP-MOD-OC8
    
    * Fix IP Blinds
    
    * Add support for HM-LC-DW-WM (#281)
    
    * Fix typo, switch to attributes
    
    * Revert IPKeyBlind
    
    * Adds class ColdWarmDimmer for HM-LC-DW-WM  (#282)
    
    * Add support for HM-LC-DW-WM
    
    * Support temp color
    
    * Move max and min mireds to ha integration
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update actors.py
    
    * Update changelog
    
    * Lint
    
    * Update changelog
    
    * Added LOWBAT Support for HM-Sec-SD (#285)
    
    * Update sensors.py (#284)
    
    Tested with HMW-Sen-SC-12-DR and Home Assistant 0.104.3
    Before the change the sensor was not discovered.
    
    * HmIP thermostat attributes and smoke detector voltage fix (#287)
    
    * Add RSSI_DEVICE and OPERATING_VOLTAGE for HmIP thermostats
    
    * Remove OPERATING_VOLTAGE from HmIP Smoke Detectors
    
    * Changed LOWBAT attribute to channel 0 globally (#288)
    
    * Add support for HmIP-WTH-B (#289)
    
    Add support for this device by adding the device type to the list in thermostat.py.
    The data points are already supported by IPThermostatWall2.
    
    * Add HmIP-DRBLI4
    
    * Bump version, update changelog
    
    Co-authored-by: Vegetto <github@angelnu.com>
    Co-authored-by: Felix Krause <felix.krause@udo.edu>
    Co-authored-by: dk1305hb <59390312+dk1305hb@users.noreply.github.com>
    Co-authored-by: hanzoh <3061232+hanzoh@users.noreply.github.com>
    Co-authored-by: Stefan Rehlig <rehlig@lemonage.de>

--- pyhomematic/devicetypes/actors.py
+++ pyhomematic/devicetypes/actors.py
@@ -86,6 +86,16 @@ class IPKeyBlind(KeyBlind):
         return [4]
 
 
+class IPKeyBlindMulti(KeyBlind):
+    """
+    Multi-blind actor HmIP-DRBLI4
+    """
+
+    @property
+    def ELEMENT(self):
+        return [10, 14, 18, 22]
+
+
 class IPKeyBlindTilt(IPKeyBlind, HelperActorBlindTilt):
 
     def close_slats(self, channel=None):
@@ -335,8 +345,7 @@ class KeyMatic(HMActor, HelperActorState, HelperRssiPeer):
         # init metadata
         self.ACTIONNODE.update({"OPEN": self.ELEMENT})
         self.ATTRIBUTENODE.update({"STATE_UNCERTAIN": self.ELEMENT,
-                                   "ERROR": self.ELEMENT,
-                                   "LOWBAT": [0]})
+                                   "ERROR": self.ELEMENT})
 
     def is_unlocked(self, channel=None):
         """ Returns True if KeyMatic is unlocked. """
@@ -758,6 +767,7 @@ DEVICETYPES = {
     "HmIP-FROLL": IPKeyBlind,
     "HmIP-BBL": IPKeyBlindTilt,
     "HmIP-FBL": IPKeyBlindTilt,
+    "HmIP-DRBLI4": IPKeyBlindMulti,
     "HM-LC-Dim1L-Pl": Dimmer,
     "HM-LC-Dim1L-Pl-2": Dimmer,
     "HM-LC-Dim1L-Pl-3": Dimmer,
