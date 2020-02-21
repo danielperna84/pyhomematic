47bcccbdcbf8346409d47b8e2f5e4f6f2aa0fed6
macrojames <482151+macrojames@users.noreply.github.com>
2020-02-21 10:04:30 +0100

    Add support for HmIP-WRCD

--- pyhomematic/devicetypes/misc.py
+++ pyhomematic/devicetypes/misc.py
@@ -56,6 +56,8 @@ class Remote(HMEvent, HelperEventRemote, HelperActionPress, HelperRssiPeer):
             return [1, 2, 3, 4]
         if "HmIP-RC8" in self.TYPE:
             return [1, 2, 3, 4, 5, 6, 7, 8]
+        if "HmIP-WRCD" in self.TYPE:
+            return [1, 2, 3]
         return [1]
 
 
@@ -136,6 +138,7 @@ DEVICETYPES = {
     "HMW-IO-4-FM": Remote,
     "HMIP-WRC2": RemoteBatteryIP,
     "HmIP-WRC2": RemoteBatteryIP,
+    "HmIP-WRCD": RemoteBatteryIP,
     "HmIP-BRC2": Remote,
     "HmIP-WRC6": RemoteBatteryIP,
     "HmIP-KRCA": Remote,
