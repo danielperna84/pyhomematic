195201ca404fc888caaa85a5f835b24c70fa909c
emufan <emufan@users.noreply.github.com>
2021-10-08 15:35:28 +0200

    Add new HMIP Evo (HmIP-eTRV-E)

--- pyhomematic/devicetypes/thermostats.py
+++ pyhomematic/devicetypes/thermostats.py
@@ -470,6 +470,7 @@ DEVICETYPES = {
     "HmIP-eTRV-B1": IPThermostat,
     "HmIP-eTRV-C": IPThermostat,
     "HmIP-eTRV-C-2": IPThermostat,
+    "HmIP-eTRV-E": IPThermostat,
     "Thermostat AA": IPThermostat,
     "Thermostat AA GB": IPThermostat,
     "HmIP-STHD": IPThermostatWall2,
